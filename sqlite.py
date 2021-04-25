1d5a48c93888d3abe409db8d4b4fefef7bfe9c53
Daniel Sorokin <40477537+mrBeardBee@users.noreply.github.com>
2021-04-25 19:36:30 -0400

    Add files via upload

---
+++ sqlite.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+con = sqlite3.connect('user_info.db')
+
+c = con.cursor()
+
+def menu():
+    menu_select = input("""Please Select an Option:
+1: Input the info
+2: Grab the Info
+> """)
+    if menu_select == '1':
+        info_input()
+    elif menu_select == '2':
+        find_email()
+    else:
+        print("Did not give a valid option.")
+        exit()
+
+
+def info_input():
+    email_input = input("Email for the account: ")
+
+    pass_input = hash(input("Password for the account: "))
+
+    c.execute(f"INSERT INTO test VALUES ('{email_input}', '{pass_input}')")
+
+    con.commit()
+
+    con.close()
+
+def find_email():
+    email_finder = input("Email that is attached to the account: ")
+
+    c.execute(f"SELECT * FROM test WHERE email='{email_finder}'")
+
+    print(c.fetchall())
+
+    con.commit()
+
+    con.close()
+
+menu()
