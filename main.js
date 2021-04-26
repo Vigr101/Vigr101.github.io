e9de5ec47fe693e76a3678b775340176a45af16f
Daniel Sorokin <40477537+mrBeardBee@users.noreply.github.com>
2021-04-25 20:31:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function save_info() {
+	alert("save_info() ran")
 	var username = document.getElementById('email').value
 	var password = document.getElementById('password').value
-
-  console.log("Info " + email + " Saved")
+	console.log("Info " + email + " Saved")
 }
