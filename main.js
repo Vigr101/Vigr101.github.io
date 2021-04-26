3543cf223996db35bf8b5034544e96268b1b6b44
Daniel Sorokin <40477537+mrBeardBee@users.noreply.github.com>
2021-04-25 20:40:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,7 @@ async function sha256(message) {
 }
 
 function save_info() {
-	alert("save_info() ran")
 	var username = document.getElementById('email').value
 	var password = document.getElementById('password').value
-	console.log("Info " + email + " Saved")
+	alert("Info " + email + ", " + password + " Saved")
 }
